547a3264b045a780ba37d2ef898cd7546cbb2575
Shimraz Rabbani <shimraz.asr.92@gmail.com>
2022-03-24 11:59:49 +0100

    Create application.py

---
+++ application.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 11:57:49 2022
+
+@author: Shimraz
+"""
+
+from kivy.uix.screenmanager import Screen
+
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRectangleFlatButton
+
+
+class MainApp(MDApp):
+    def build(self):
+        screen = Screen()
+        screen.add_widget(
+            MDRectangleFlatButton(
+                text="Hello, World",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            )
+        )
+        return screen
+
+
+MainApp().run()
