ff5cf5f1dd25f3b62360641dae33f63cfcc8d290
Shimraz Rabbani <shimraz.asr.92@gmail.com>
2022-03-24 14:14:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.storage.jsonstore import JsonStore
-data = JsonStore('data.json')
+
 
 # Replace this with your
 # current version
@@ -28,6 +28,7 @@ class MyFirstKivyApp(App):
     # Function that returns
     # the root widget
     def build(self):
+<<<<<<< HEAD
         # Label with text Hello World is
         # returned as root widget
         receiptOcrEndpoint = 'https://ocr.asprise.com/api/v1/receipt'  # Receipt OCR API endpoint
@@ -61,12 +62,23 @@ print(r.text) # result in JSON
             name = de.split('x')[1].split('à')[0]
             data.put(name, quantity=quantity)
             print(name)
+=======
+        data = JsonStore('data.json')
+>>>>>>> b8d878321948c3d946381e736613cdc7547f8682
 
         data.put("bananas", quantity=2)
         data.put("apples", quantity=4)
 
-        for key, data in data:
-            print(key, ": ", data)
+        url = "https://api.mindee.net/v1/products/mindee/expense_receipts/v3/predict"
+
+        with open("receipt.png", "rb") as myfile:
+            files = {"document": myfile}
+            headers = {"Authorization": "Token 0239a53e8839c150bf8d3b71d5055c21"}
+            response = requests.post(url, files=files, headers=headers)
+            print(response.text)
+
+        for key in data:
+            print(key, ": ", data.get(key)['quantity'])
 
         return Label(text="Hello World !")
 
